341ca368b718fda4fd40348acd568167e9d99015
Felix Kratz <felix.kratz@tu-dortmund.de>
2025-06-15 21:20:32 +0200

    simplify test program

--- simulation.cpp
+++ simulation.cpp
@@ -4,34 +4,18 @@ int sc_main(int argc, char* argv[]) {
   Simulation simulation(true);
 
   mem_addr_t counter = 0;
-  mem_t memory;
-  // lda #2
-  memory[counter++] = OP_LDA_IMM;
-  memory[counter++] = 2;
-
-  // sta $0xf0
-  memory[counter++] = OP_STA_ZPG;
-  memory[counter++] = 0xf0;
-
-  // lda $0xf0
-  memory[counter++] = OP_LDA_ZPG;
-  memory[counter++] = 0xf0;
-
-  // Write jmp 0xa
-  memory[counter++] = OP_JMP_ABS;
-  memory[counter++] = counter + 0x2;
-  memory[counter++] = 0;
-
-  // Write nop slide
-  memory[counter++] = OP_NOP;
-  memory[counter++] = OP_NOP;
-  memory[counter++] = OP_NOP;
-
-  // Write stp
-  memory[counter++] = OP_BRK;
+  mem_t memory = {
+    OP_LDA_IMM, 0x2,
+    OP_STA_ZPG, 0xf0,
+    OP_LDA_ZPG, 0xf0,
+    OP_JMP_ABS, 0xb, 0,
+    OP_NOP,
+    OP_NOP,
+    OP_NOP,
+    OP_BRK,
+  };
 
   simulation.memory.set_memory(std::move(memory));
-
   simulation.step(100);
   return 0;
 }
